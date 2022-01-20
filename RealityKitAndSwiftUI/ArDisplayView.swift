41428741e8b019e47eee66a2b7f132c48c09edc2
Vlad <vlad@unpi.ro>
2022-01-20 08:29:44 +0200

    Add .gitignore file

---
+++ RealityKitAndSwiftUI/ArDisplayView.swift
@@ -0,0 +1,20 @@
+//
+//  ArDisplayView.swift
+//  RealityKitAndSwiftUI
+//
+//  Created by Vlad Toma on 18.01.2022.
+//
+
+import SwiftUI
+
+struct ArDisplayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArDisplayView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArDisplayView()
+    }
+}
---
+++ RealityKitAndSwiftUI/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  RealityKitAndSwiftUI
+//
+//  Created by Vlad Toma on 18.01.2022.
+//
+
+import Foundation
